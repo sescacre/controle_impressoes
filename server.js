7170004ca29b5500b0f00a0ff3cfbb82bdb95924
silasgoncalvesczs <silassilvaczs@hotmail.com>
2026-09-22 18:34:39 -0500

    alteração de porta para 3400

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 3400;
 const ROOT = __dirname;
 
 const MIME_TYPES = {
